288fea8a8930c162424ef5964537615d00b6e58d
soniaM7 <sonia.miglani33@gmail.com>
2019-12-03 22:49:27 +0100

    03 Nov

---
+++ com/automation/assignment1.py
@@ -0,0 +1,19 @@
+input_list=[-2,89,8,9]
+
+print(input_list)
+
+
+def bubble_sort(initial):
+    temp=0
+    print(initial)
+    for i in range(len(initial)):
+        for j in range(i):
+            if initial[i] < initial[j]:
+                temp = initial[j]
+                initial[j] = initial[i]
+                initial[i] = temp
+
+    return initial
+
+outputlist=bubble_sort(input_list)
+print (outputlist)
